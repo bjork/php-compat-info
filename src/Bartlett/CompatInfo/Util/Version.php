857da231654bfa7250eba0f1b7f7cea85eee388c
Laurent Laville <laurent.laville@gmail.com>
2015-05-06 13:24:08 +0200

    normalizes version number (int format) and text (dotted format) for each lib requires by extensions

--- src/Bartlett/CompatInfo/Util/Version.php
+++ src/Bartlett/CompatInfo/Util/Version.php
@@ -50,4 +50,75 @@ class Version
         }
         return '';
     }
+
+    public static function lib($name, $key = 'version_number')
+    {
+        if ('curl' == $name
+            && function_exists('curl_version')
+        ) {
+            $meta = curl_version();
+            $meta['version_text'] = $meta['version'];
+
+        } elseif ('libxml' == $name) {
+            $meta = array(
+                'version_number' => defined('LIBXML_DOTTED_VERSION')
+                    ? self::toNumber(LIBXML_DOTTED_VERSION) : false,
+                'version_text'   => defined('LIBXML_DOTTED_VERSION')
+                    ? LIBXML_DOTTED_VERSION : false,
+            );
+
+        } elseif ('intl' == $name) {
+            $meta = array(
+                'version_number' => defined('INTL_ICU_VERSION')
+                    ? self::toNumber(INTL_ICU_VERSION) : false,
+                'version_text'   => defined('INTL_ICU_VERSION')
+                    ? INTL_ICU_VERSION : false,
+            );
+
+        } elseif ('openssl' == $name) {
+            $meta = array(
+                'version_number' => defined('OPENSSL_VERSION_NUMBER')
+                    ? OPENSSL_VERSION_NUMBER : false,
+                'version_text'   => defined('OPENSSL_VERSION_TEXT')
+                    ? self::toText(OPENSSL_VERSION_NUMBER) : false,
+            );
+        }
+        if (isset($meta)) {
+            if (isset($key) && array_key_exists($key, $meta)) {
+                return $meta[$key];
+            }
+            return $meta;
+        }
+        return false;
+    }
+
+    protected static function toText($number)
+    {
+        $hex = dechex(($number & ~ 15) / 16);
+
+        if (strlen($hex) % 2 !== 0) {
+            $hex = '0' . $hex;
+        }
+
+        $arr = str_split($hex, 2);
+
+        return implode('.', array_map('hexdec', $arr));
+    }
+
+    protected static function toNumber($text)
+    {
+        $arr = explode('.', $text);
+        $arr = array_map('dechex', $arr);
+        $hex = '';
+
+        foreach ($arr as $digit) {
+            if (strlen($digit) % 2 !== 0) {
+                $hex .= '0';
+            }
+            $hex .= $digit;
+        }
+        $hex .= 'F';
+
+        return hexdec($hex);
+    }
 }
